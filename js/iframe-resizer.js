5ad55abba98ce9f7d3fc0e1f265076246081c888
Adrian Tirante <16868065+onotype@users.noreply.github.com>
2023-01-27 19:21:03 +0800

    Added a log to the resizer

--- js/iframe-resizer.js
+++ js/iframe-resizer.js
@@ -1,3 +1,5 @@
 document.querySelector('iframe').onload = function() {
   obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
 }
+
+console.log("Resizer is loading.");
