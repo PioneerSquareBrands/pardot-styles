50a40f542c02f38405ac2b977e361b41234f1b5b
Adrian Tirante <16868065+onotype@users.noreply.github.com>
2023-01-30 22:36:26 +0800

    Added fonts using @import. Revisit this

--- js/iframe-resizer.js
+++ js/iframe-resizer.js
@@ -1,5 +1,5 @@
 document.querySelector('iframe').onload = function() {
-  document.querySelector('iframe').style.height = document.querySelector('iframe').contentWindow.document.documentElement.scrollHeight + 'px';
+  document.querySelector('iframe').style.height = document.querySelector('iframe').contentWindow.document.documentElement.scrollHeight - 350 + 'px';
 }
 
 console.log("Resizer is loading.");
